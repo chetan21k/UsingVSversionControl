7378688d08adcbc5305fb0b4f252e04598da2fb4
chetan.kothari <chetan.kothari@IN-DT-CHETAN>
2019-12-17 15:25:19 +0530

    [self-2] function created

--- GitRepositoryForVersionControl/GitRepositoryForVersionControl/Program.cs
+++ GitRepositoryForVersionControl/GitRepositoryForVersionControl/Program.cs
@@ -4,14 +4,13 @@ namespace GitRepositoryForVersionControl
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void OddEven()
         {
-            Console.WriteLine("Program To Odd Even from 1 to 500");
             int[] odd = new int[250];
             int[] even = new int[250];
-            for(int i = 1,j=0,k=0; i < 501; i++)
+            for (int i = 1, j = 0, k = 0; i < 501; i++)
             {
-                if(i%2==0)
+                if (i % 2 == 0)
                 {
                     even[j++] = i;
                 }
@@ -19,13 +18,20 @@ namespace GitRepositoryForVersionControl
                 {
                     odd[k++] = i;
                 }
-                
+
             }
             Console.WriteLine(" Even List:");
             Console.WriteLine(string.Join(",", even));
 
             Console.WriteLine(" Odd List:");
             Console.WriteLine(string.Join(",", odd));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program To Odd Even from 1 to 500");
+            OddEven();
+
+
         }
     }
 }
